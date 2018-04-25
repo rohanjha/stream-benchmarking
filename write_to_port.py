0d4619e1bb2e9e3bff2ed48b60421b2aa601c5ff
rohanjha <rohan_jha@brown.edu>
2018-04-25 16:28:05 -0400

    write_to_port acts more like a server now

--- write_to_port.py
+++ write_to_port.py
@@ -1,14 +1,22 @@
 import socket
 import time
 import numpy as np
+import sys
+
+TCP_PORT = 9999
+if (len(sys.argv) > 1):
+	TCP_PORT = int(sys.argv[1])
 
 TCP_IP = 'localhost'
-TCP_PORT = 9999
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+s = socket.socket()
+s.bind((TCP_IP, TCP_PORT))
+s.listen(5)
+c, addr = s.accept()
 
+print("Connection accepted from " + repr(addr[1]))
 while True:
-    rand_int = np.random.normal()
-    s.send((str(rand_int) + "\n").encode())
     time.sleep(0.05)
+    c, addr 
+    rand_int = np.random.normal()
+    c.send((str(rand_int) + "\n").encode())
