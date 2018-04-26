8f9306d81481252b62c5dc86e268c4cc0d00cc40
rohanjha <rohan_jha@brown.edu>
2018-04-26 00:38:02 -0400

    trying to send data over tcp to database -- not working yet

--- streaming_test.py
+++ streaming_test.py
@@ -2,19 +2,30 @@ from __future__ import print_function
 
 import sys
 import time
-import sqlite3
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
+import socket
+import pickle
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # sanity check
 def print_dp(param):
     if (int((param[0].split(" "))[0]) % 100 == 0):
         print(str(param[0]) + " " + str(param[1]) + " " + str(param[2]) + " " + str(param[3]))
 
+def send_rdd_to_db(rdd, out_port):
+    conn = socket.create_connection(('localhost', out_port))
+    rdd.foreach(lambda record: conn.send(pickle.dumps(record)))
+    conn.close()
+
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: network_wordcount.py <hostname> <port>", file=sys.stderr)
+    if len(sys.argv) != 4:
+        print("Usage: network_wordcount.py <host name> <in port> <local out port>", file=sys.stderr)
         exit(-1)
+
+    out_port = int(sys.argv[3])
+
     sc = SparkContext(appName="PythonStreamingNetworkWordCount")
     ssc = StreamingContext(sc, 1)
 
@@ -22,9 +33,9 @@ if __name__ == "__main__":
     w_1_ts = buckets.map(lambda val: [val, str(int(round(time.time() * 1e8)))]) # adding pre-processing timestamp
     w_counts = w_1_ts.map(lambda val: [val[0], val[1], abs(float(val[0].split(" ")[1])) > 2]) # processing
     w_2_ts = w_counts.map(lambda val: [val[0], val[1], str(int(round(time.time() * 1e8))), val[2]]) # adding post-processing and pre-storage timestamp
-    w_2_ts.saveAsTextFiles("text") # storing
+    w_2_ts.foreachRDD(lambda rdd: send_rdd_to_db(rdd, out_port)) # storing
 
-    # storage side will take care of the pos-storage timestamps
+    # storage side will take care of the post-storage timestamps
 
     # sanity check
     w_2_ts.foreachRDD(lambda rdd: rdd.foreach(print_dp))
