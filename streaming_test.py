fe3129cad5c98a351ad0803bd809e4f48065d00c
rohanjha <rohan_jha@brown.edu>
2018-04-26 02:05:10 -0400

    mostly working w tcp -- will try udp

--- streaming_test.py
+++ streaming_test.py
@@ -7,17 +7,18 @@ from pyspark.streaming import StreamingContext
 import socket
 import pickle
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # sanity check
 def print_dp(param):
     if (int((param[0].split(" "))[0]) % 100 == 0):
         print(str(param[0]) + " " + str(param[1]) + " " + str(param[2]) + " " + str(param[3]))
 
-def send_rdd_to_db(rdd, out_port):
-    conn = socket.create_connection(('localhost', out_port))
-    rdd.foreach(lambda record: conn.send(pickle.dumps(record)))
-    conn.close()
+def send_partition_to_db(partition, out_port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', out_port))
+    for record in partition:
+        sock.send(pickle.dumps(record))
+    sock.send("end")
+    sock.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -33,7 +34,7 @@ if __name__ == "__main__":
     w_1_ts = buckets.map(lambda val: [val, str(int(round(time.time() * 1e8)))]) # adding pre-processing timestamp
     w_counts = w_1_ts.map(lambda val: [val[0], val[1], abs(float(val[0].split(" ")[1])) > 2]) # processing
     w_2_ts = w_counts.map(lambda val: [val[0], val[1], str(int(round(time.time() * 1e8))), val[2]]) # adding post-processing and pre-storage timestamp
-    w_2_ts.foreachRDD(lambda rdd: send_rdd_to_db(rdd, out_port)) # storing
+    w_2_ts.foreachRDD(lambda rdd: rdd.foreachPartition(lambda partition: send_partition_to_db(partition, out_port))) # storing
 
     # storage side will take care of the post-storage timestamps
 
