65ded9555546ae6b00f7ef6d5045355f1f2e1d0f
rohanjha <rohan_jha@brown.edu>
2018-04-26 03:24:15 -0400

    finishing up framework

--- streaming_test.py
+++ streaming_test.py
@@ -13,9 +13,12 @@ def print_dp(param):
         print(str(param[0]) + " " + str(param[1]) + " " + str(param[2]) + " " + str(param[3]))
 
 def send_partition_to_db(partition, out_port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    list_of_records = []
     for record in partition:
-        sock.sendto(pickle.dumps(record), ('localhost', out_port))
+        list_of_records.append(record)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(pickle.dumps(list_of_records), ('localhost', out_port))
     sock.close()
 
 if __name__ == "__main__":
@@ -30,7 +33,7 @@ if __name__ == "__main__":
 
     buckets = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
     w_1_ts = buckets.map(lambda val: [val, str(int(round(time.time() * 1e8)))]) # adding pre-processing timestamp
-    w_counts = w_1_ts.map(lambda val: [val[0], val[1], abs(float(val[0].split(" ")[1])) > 2]) # processing
+    w_counts = w_1_ts.map(lambda val: [val[0], val[1], int(val[0].split(" ")[1]) > 22000]) # processing
     w_2_ts = w_counts.map(lambda val: [val[0], val[1], str(int(round(time.time() * 1e8))), val[2]]) # adding post-processing and pre-storage timestamp
     w_2_ts.foreachRDD(lambda rdd: rdd.foreachPartition(lambda partition: send_partition_to_db(partition, out_port))) # storing
 
--- write_to_port.py
+++ write_to_port.py
@@ -2,6 +2,7 @@ import socket
 import time
 import numpy as np
 import sys
+import csv
 
 TCP_PORT = 9999
 if (len(sys.argv) > 1):
@@ -9,16 +10,23 @@ if (len(sys.argv) > 1):
 
 TCP_IP = 'localhost'
 
+f = open("nyc_taxi-train.csv")
+reader = csv.reader(f)
+
+data = []
+for row in reader:
+	data.append(row[2])
+data.pop(0)
+
 s = socket.socket()
 s.bind((TCP_IP, TCP_PORT))
 s.listen(5)
 c, addr = s.accept()
 
 print("Connection accepted from " + repr(addr[1]))
-id = 0
+index = 0
 while True:
     c, addr
-    rand_int = np.random.normal()
-    c.send(str(id) + " " + (str(rand_int) + "\n").encode())
+    c.send((str(index) + " " + str(data[index % len(data)]) + "\n").encode())
     time.sleep(0.01)
-    id += 1
+    index += 1
