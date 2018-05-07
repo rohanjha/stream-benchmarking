f834e98440d64c3da4b75c34d3abaaeec7751a73
rohanjha <rohan_jha@brown.edu>
2018-05-07 19:26:01 -0400

    changes to dataset -- things no longer working

--- write_to_port.py
+++ write_to_port.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import sys
 import csv
+import pickle
 
 TCP_PORT = 9999
 if (len(sys.argv) > 1):
@@ -10,13 +11,13 @@ if (len(sys.argv) > 1):
 
 TCP_IP = 'localhost'
 
-f = open("data/nyc_taxi-train.csv")
+# not ideal but hardcoded for now
+f = open("data/testing.csv")
 reader = csv.reader(f)
 
 data = []
 for row in reader:
-	data.append(row[2])
-data.pop(0)
+	data.append([row[0], row[1], row[2], row[3], row[4], row[5]])
 
 s = socket.socket()
 s.bind((TCP_IP, TCP_PORT))
@@ -27,6 +28,10 @@ print("Connection accepted from " + repr(addr[1]))
 index = 0
 while True:
     c, addr
-    c.send((str(index) + " " + str(data[index % len(data)]) + "\n").encode())
+    dp = data[index % len(data)]
+    dp.insert(0, index)
+    print(pickle.dumps(dp))
+
+    c.send(str(dp))
     time.sleep(0.01)
     index += 1
