50442b16c991e2fb49d43295f4d70f6e09cda3c4
rohanjha <rohan_jha@brown.edu>
2018-05-09 14:42:51 -0400

    finishing up -- added kmeans-streaming and did some cleaning

--- stream.py
+++ stream.py
@@ -14,9 +14,9 @@ from pyspark.mllib.classification import LogisticRegressionWithLBFGS, LogisticRe
 from pyspark.mllib.regression import LabeledPoint
 
 from pyspark.mllib.clustering import KMeans, KMeansModel
+from pyspark.mllib.clustering import StreamingKMeans, StreamingKMeansModel
 
 testing = False
-model = None
 
 if (testing):
     print_interval = 100
@@ -32,6 +32,7 @@ def get_labeled_points():
             for i in range(len(row) - 1):
                 attributes.append(float(row[i]))
             train_data.append(LabeledPoint(int(row[len(row) - 1]), attributes))
+            print(attributes)
     return train_data
 
 def get_attributes():
@@ -45,7 +46,6 @@ def get_attributes():
             train_data.append(attributes)
     return train_data
 
-
 # training the model if training is required
 def train(model_name, sc):
     if (model_name == "logistic"):
@@ -53,25 +53,39 @@ def train(model_name, sc):
         model = LogisticRegressionWithLBFGS.train(sc.parallelize(train_data))
         print("Done training.")
         print("Sample prediction: " + str(model.predict([0, 0, 0, 0, 0, 9])))
+        return model
     elif (model_name == "kmeans"):
         train_data = get_attributes()
         model = KMeans.train(sc.parallelize(train_data), 2, maxIterations=10, initializationMode="random")
         print("Done training.")
         print(model.clusterCenters)
-    elif (not model_name == "baseline"):
+        return model
+    elif (model_name == "kmeans-streaming"):
+        initCenters = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]]
+        initWeights = [1.0, 1.0]
+        model = StreamingKMeansModel(initCenters, initWeights)
+        print("Created model.")
+        return model
+        # the training is done as the data comes in
+    elif (not (model_name == "baseline" or model_name == "kmeans-streaming")):
         print("Model not implemented", file=sys.stderr)
         exit(-1)
 
+    return None
+
+def update_kmeans(rdd, model):
+    model = model.update(rdd, 1.0, "batches")
+
 # processing the data
-def process(val, model_name):
+def process(val, model_name, model):
     if (model_name == "baseline"):
         return val[1] > 0.8
     elif (model_name == "logistic"):
         return model.predict(val[1:len(val)-1])
-    elif (model_name == "kmeans"):
+    elif (model_name == "kmeans" or model_name == "kmeans-streaming"):
         attributes = np.array(val[1:len(val)-1])
         min_distance = np.min(np.linalg.norm(model.clusterCenters - attributes, axis=1))
-        return min_distance >= 0.8
+        return int(min_distance >= 0.8)
 
     # shouldn't get here
     return 0
@@ -102,12 +116,18 @@ if __name__ == "__main__":
     ssc = StreamingContext(sc, 1)
 
     # wil also verify the model name
-    train(model_name, sc)
+    model = train(model_name, sc)
 
     buckets = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
-    buckets = buckets.map(lambda val: ast.literal_eval(val)) # converting to strings
+    buckets = buckets.map(lambda val: ast.literal_eval(val)) # converting to lists
     buckets = buckets.map(lambda val: val + [int(round(time.time() * 1e8))]) # adding pre-processing timestamp
-    buckets = buckets.map(lambda val: val + [process(val, model)]) # processing
+
+    # we need to train if the model is streaming
+    if (model_name == "kmeans-streaming"):
+        training_stream = buckets.map(lambda val: val[1:len(val)-1])
+        training_stream.foreachRDD(lambda rdd: update_kmeans(rdd, model))
+
+    buckets = buckets.map(lambda val: val + [process(val, model_name, model)]) # processing
     buckets = buckets.map(lambda val: val + [int(round(time.time() * 1e8))])  # adding post-processing timestamp
     buckets.foreachRDD(lambda rdd: rdd.foreachPartition(lambda partition: send_partition_to_db(partition, out_port))) # storage
 
